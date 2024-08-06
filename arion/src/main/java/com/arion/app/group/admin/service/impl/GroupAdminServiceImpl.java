a7d340ee6c10c0faa7f0b7d693110530d9cb6521
ass6728 <ass6728@naver.com>
2024-08-07 02:30:38 +0900

    사이드바에 주소값 동적으로 넣었어 .. 이거 상당히 어려워

--- arion/src/main/java/com/arion/app/group/admin/service/impl/GroupAdminServiceImpl.java
+++ arion/src/main/java/com/arion/app/group/admin/service/impl/GroupAdminServiceImpl.java
@@ -1,8 +1,25 @@
 package com.arion.app.group.admin.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.arion.app.group.admin.mapper.GroupAdminMapper;
+import com.arion.app.group.admin.service.EmployeeVO;
+import com.arion.app.group.admin.service.GroupAdminService;
+
 @Service
-public class GroupAdminServiceImpl {
+public class GroupAdminServiceImpl implements GroupAdminService{
+
+	private GroupAdminMapper gaMapper;
+	
+	public GroupAdminServiceImpl(GroupAdminMapper groupAdminMapper) {
+		this.gaMapper = groupAdminMapper;
+	}
+	
+	@Override
+	public List<EmployeeVO> empListSelect(String companyCode) {
+		return gaMapper.selectempList(companyCode);
+	}
 
 }

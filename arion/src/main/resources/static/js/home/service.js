7e82e90c4f8bde037078163dee396558faf65846
kimdogyeom <kdg412@naver.com>
2024-08-07 14:31:01 +0900

    2024 08 07 서비스 견적 테스트

---
+++ arion/src/main/resources/static/js/home/service.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+	function calculateAmounts() {
+		let accountNumber = parseInt($('#accountNumber').val());
+		let period = parseInt($('#subscriptionPeriod').val());
+		let checkedModules = $('.module-checkbox:checked');
+		let accountAmount = accountNumber * 1200 * (period/30);
+		let payDate = ($('#regularPaymentDate').val());
+		let moduleAmount = 0 ;
+
+
+		checkedModules.each(function() {
+			moduleAmount += parseInt($(this).data('price') * (period / 30));
+		});
+	
+		let monthlyAmount = accountAmount + moduleAmount;
+		let firstmonthAmount;
+		let totalAmount = monthlyAmount * (period / 30);
+
+		$('#accountAmount').text(accountAmount.toLocaleString().split(".")[0] + '원');
+		$('#monthlyAmount').text(monthlyAmount.toLocaleString().split(".")[0] + '원');
+		$('#totalAmount').text(totalAmount.toLocaleString().split(".")[0] + '원');
+		$('#moduleAmount').text(moduleAmount.toLocaleString().split(".")[0] + '원');
+		
+	}
+		
+	$('#accountNumber').change(function() {
+		if($(this).val() > 100){
+			Swal.fire({
+				icon: "error",
+				text: "100인 초과는 상담직원에게 문의하세요."
+			});
+			$(this).val() == 100;
+		}
+		calculateAmounts();
+   	});
+
+	$('#subscriptionPeriod').change(function() {
+		if($(this).val() > 30) {
+			$('#regularPaymentDate').prop('disabled', false).val(10);
+		}else{
+			$('#regularPaymentDate').prop('disabled', true).val('');
+		}
+		
+		if($(this).val() > 1825) {
+			Swal.fire({
+				icon: "error",
+				text: "5년 초과는 상담직원에게 문의하세요."
+			});
+			$(this).val() == 1825;
+		}
+		calculateAmounts();
+	});
+	
+	$('.module-checkbox').change(calculateAmounts);
+	
+	calculateAmounts();
+})

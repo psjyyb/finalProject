8a41e9112b89da911693477cd10235067c8407c8
alstjd506 <kdg412@naver.com>
2024-08-21 06:06:16 +0900

    2024 08 21 충돌..처리..

---
+++ arion/src/main/java/com/arion/app/group/main/approval/service/SignVO.java
@@ -0,0 +1,14 @@
+package com.arion.app.group.main.approval.service;
+
+import lombok.Data;
+
+@Data
+public class SignVO {
+	private int singNo;
+	private int employeeNo;
+	private String signImg;
+	private int apprNo;
+	private int docNo;
+	private String companyCode;
+	private String employeeId;
+}

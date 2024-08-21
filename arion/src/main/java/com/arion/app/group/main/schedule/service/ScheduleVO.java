bafe7040835a89095996905880cd1686445e3644
psjyyb <ass6728@naver.com>
2024-08-21 17:53:55 +0900

    일정관리

--- arion/src/main/java/com/arion/app/group/main/schedule/service/ScheduleVO.java
+++ arion/src/main/java/com/arion/app/group/main/schedule/service/ScheduleVO.java
@@ -1,5 +1,22 @@
 package com.arion.app.group.main.schedule.service;
 
-public class ScheduleVO {
+import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class ScheduleVO {
+	private int scheduleNo;
+	private String scheduleTitle;
+	@JsonFormat(pattern = "yyyy-MM-dd hh:mm")
+	private LocalDate scheduleStart;
+	@JsonFormat(pattern = "yyyy-MM-dd hh:mm")
+	private LocalDate scheduleEnd;
+	private int employeeNo;
+	private String companyCode;
+	private int departmentsNo;
+	private String scheduleStarts;
+	private String scheduleEnds;
 }
--- arion/src/main/java/com/arion/app/group/main/schedule/web/ScheduleController.java
+++ arion/src/main/java/com/arion/app/group/main/schedule/web/ScheduleController.java
@@ -1,7 +1,15 @@
 package com.arion.app.group.main.schedule.web;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.arion.app.group.main.schedule.service.ScheduleVO;
 
 @Controller
 public class ScheduleController {
@@ -10,5 +18,13 @@ public class ScheduleController {
 	public String ScheduleCalendar() {
 		
 		return"/group/schedule/calendar";
+	}
+	@PostMapping("/group/schedule/empCalcInsert")
+	@ResponseBody
+	public Map<String,Object> empCalcInsert(ScheduleVO scheduleVO,HttpSession session){
+		String comCode = (String) session.getAttribute("companyCode");
+		int empNo = (Integer) session.getAttribute("employeeNo");
+		System.out.println(scheduleVO);
+	return null;
 	}
 }

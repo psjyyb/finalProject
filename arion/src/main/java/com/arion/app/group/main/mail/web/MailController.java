02ac04eb273d6003d3e185171b0a5c1f3d2686cf
Bae Dong Gyu <qoehdrb86@gmail.com>
2024-08-27 12:33:05 +0900

    Merge branch 'Bae-Dong-Gyu' 0827

--- arion/src/main/java/com/arion/app/group/main/mail/web/MailController.java
+++ arion/src/main/java/com/arion/app/group/main/mail/web/MailController.java
@@ -239,6 +239,8 @@ public class MailController {
             mailService.updateMailStatus(mailVO.getMailIds(), employeeId, "TRASH");
         } else if ("RECEIVED".equals(mailVO.getAction())) {
             mailService.updateMailStatus(mailVO.getMailIds(), employeeId, "RECEIVED");
+        } else if ("DELETE".equals(mailVO.getAction())) {
+            mailService.updateMailStatus(mailVO.getMailIds(), employeeId, "DELETE");
         }
         
         return ResponseEntity.ok("Success");
@@ -247,17 +249,24 @@ public class MailController {
     // 메일 상태 변경 핸들러 수정
     @PostMapping("/removes")
     public ResponseEntity<String> handleMail(@RequestBody MailVO mailVO) {
+        System.out.println("왔냐?");
         System.out.println("Action: " + mailVO.getAction()); // 액션 값 출력
         System.out.println("Mail IDs: " + mailVO.getMailIds()); // 메일 ID 출력
 
+        // 세션에서 값 가져오기
         String employeeId = (String) httpSession.getAttribute("loginId");
+        String companyCode = (String) httpSession.getAttribute("companyCode");
+        String empNo = (String) httpSession.getAttribute("employeeNo");
+
+        System.out.println(companyCode + "dasdadaskfjasf");
+        System.out.println(empNo + "555555");
 
         if ("DELETE".equals(mailVO.getAction())) {
             // 메일 상태를 DELETE로 업데이트합니다.
-            mailService.updateMailStatus(mailVO.getMailIds(), employeeId, "DELETE");
-            System.out.println(mailVO.getAction()+"123123123123");
+            mailService.deleteMailStatus(mailVO.getMailIds(), employeeId, "DELETE");
+            System.out.println(mailVO.getAction() + "123123123123");
         } 
-        
+
         return ResponseEntity.ok("Success");
     }
 }

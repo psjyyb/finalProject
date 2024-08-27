d759bd91bee9e877f3a794a3ad00954997be5909
psjyyb <ass6728@naver.com>
2024-08-27 15:46:37 +0900

    주석지움

--- arion/src/main/java/com/arion/app/group/admin/service/impl/GroupAdminServiceImpl.java
+++ arion/src/main/java/com/arion/app/group/admin/service/impl/GroupAdminServiceImpl.java
@@ -227,7 +227,6 @@ public class GroupAdminServiceImpl implements GroupAdminService {
 		  try {
 			Workbook workbook = new XSSFWorkbook(excelFile.getInputStream());
 			Sheet sheet = workbook.getSheetAt(0);
-			System.out.println(sheet.getPhysicalNumberOfRows()+"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 			for(int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
 				Row row = sheet.getRow(i);
 				EmployeeVO evo = new EmployeeVO();
